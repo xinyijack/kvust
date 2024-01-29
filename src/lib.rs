2f496f9cd2568fea75675e0d77736c7643fe688a
jack_xin <xinyijack@qq.com>
2024-01-29 09:24:49 +0800

    陈天22课完成

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+mod pb;
+mod error;
+mod storage;
+mod service;
+
+pub use pb::abi::*;
+pub use error::KvError;
+pub use storage::*;
+pub use service::*;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
