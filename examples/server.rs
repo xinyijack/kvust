2aae96044707a206ec1f1e40491119b5f8fdbc5c
jack_xin <xinyijack@qq.com>
2024-02-04 06:27:16 +0800

    陈天37课,在 KV server 中使用 TLS 部分全部完成

--- examples/server.rs
+++ examples/server.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use async_prost::AsyncProstStream;
 use futures::prelude::*;
-use kv::{CommandRequest, CommandResponse, Service, ServiceInner};
+use kv::{CommandRequest, CommandResponse, Service, ServiceInner, TlsServerAcceptor};
 use tokio::net::TcpListener;
 use tracing::info;
 use kv::memory::MemTable;
@@ -11,15 +11,23 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let service: Service = ServiceInner::new(MemTable::default()).into();
     let addr = "127.0.0.1:9527";
+
+    // 以后从配置文件取
+    let server_cert = include_str!("../fixtures/server.cert");
+    let server_key = include_str!("../fixtures/server.key");
+
+    let acceptor = TlsServerAcceptor::new(server_cert, server_key, None)?;
+    let service: Service = ServiceInner::new(MemTable::new()).into();
     let listener = TcpListener::bind(addr).await?;
     info!("Start listening on {}", addr);
     loop {
+        let tls = acceptor.clone();
         let (stream, addr) = listener.accept().await?;
         info!("Client {:?} connected", addr);
-        let svc = service.clone();
+        let stream = tls.accept(stream).await?;
+        let stream =
         tokio::spawn(async move {
-            let mut stream =
-                AsyncProstStream::<_, CommandRequest, CommandResponse, _>::from(stream).for_async();
+
             while let Some(Ok(cmd)) = stream.next().await {
                 let res = svc.execute(cmd);
                 stream.send(res).await.unwrap();
